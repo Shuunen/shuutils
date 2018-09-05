5e0d7f4f7b2ca8a56b6a726f7f5080ff84a9ab4b
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-09-05 14:04:12 +0200

    better ts lint & conf

--- src/timestamps.ts
+++ src/timestamps.ts
@@ -1,9 +1,17 @@
 'use strict'
 
+/**
+ * Give timestamp of a date or now in seconds
+ * @param date input date
+ */
 export function getTimestamp(date: Date = new Date()): number {
   return Math.round(date.getTime() / 1000)
 }
 
+/**
+ * Give timestamp of a date or now in milliseconds
+ * @param date input date
+ */
 export function getTimestampMs(date: Date = new Date()): number {
   return date.getTime()
 }
