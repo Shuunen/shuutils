2e3230cf7e4205baf3c71355899827e5b5ad0b22
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-11-03 15:58:58 +0100

    tslint/eslint -> standard

--- src/dates.ts
+++ src/dates.ts
@@ -14,7 +14,7 @@
  * @param date input date
  * @param removeTimezone remove the last z ?
  */
-export function dateToIsoString(date: Date, removeTimezone?: boolean): string {
+export function dateToIsoString (date: Date, removeTimezone?: boolean): string {
   let dateStr = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString()
   if (removeTimezone && dateStr[dateStr.length - 1].toLowerCase() === 'z') {
     dateStr = dateStr.substr(0, dateStr.length - 1)
--- src/timestamps.ts
+++ src/timestamps.ts
@@ -4,7 +4,7 @@
  * Give timestamp of a date or now in seconds
  * @param date input date
  */
-export function getTimestamp(date: Date = new Date()): number {
+export function getTimestamp (date: Date = new Date()): number {
   return Math.round(date.getTime() / 1000)
 }
 
@@ -12,6 +12,6 @@ export function getTimestamp(date: Date = new Date()): number {
  * Give timestamp of a date or now in milliseconds
  * @param date input date
  */
-export function getTimestampMs(date: Date = new Date()): number {
+export function getTimestampMs (date: Date = new Date()): number {
   return date.getTime()
 }
