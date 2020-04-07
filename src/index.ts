b9445b9c8640ca8a31342368ec491cbdaf5a3d93
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-04-07 18:40:09 +0200

    simpler exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 'use strict'
 
-import { pickOne } from './arrays'
-import { dateToIsoString } from './dates'
-import { getRandomNumber } from './numbers'
-import { copy } from './objects'
-import { slugify, getRandomImageUrl, getRandomString } from './strings'
-import { getTimestamp, getTimestampMs } from './timestamps'
-
-export = { getTimestamp, getTimestampMs, dateToIsoString, pickOne, slugify, getRandomImageUrl, getRandomString, getRandomNumber, copy }
+export * from './arrays'
+export * from './dates'
+export * from './functions'
+export * from './numbers'
+export * from './objects'
+export * from './strings'
+export * from './timestamps'
