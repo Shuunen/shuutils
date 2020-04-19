1498c563ecfb494bfe9c4e66c44874a024ecceae
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-04-19 12:55:31 +0200

    update repo checker conf

---
+++ repo-checker.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  user_name: 'Romain Racamier-Lafon',
+  user_mail: 'romain.racamier@gmail.com',
+  license: 'MIT',
+  max_size_ko: 30, // until this lib is bundled into a single file, this is a fake check dude xD
+}
