f60d2a41e76a1dff37e9e3af12ac1cc2bd1cfb5f
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-05-06 19:50:22 +0200

    roll back max size conf

--- repo-checker.config.js
+++ repo-checker.config.js
@@ -3,4 +3,5 @@ module.exports = {
   user_mail: 'romain.racamier@gmail.com',
   license: 'MIT',
   dev_deps_only: false, // dlv is a required dependency for the user that install shuutils
+  max_size_ko: 30, // until this lib is bundled into a single file, this is a fake check xD
 }
