59d4e15a33abcc37aafef58ae1fc2ad59e447cf1
Guillaume Malek <guivome@gmail.com>
2021-04-20 17:26:02 +0200

    handle array on storage service

--- src/storage.ts
+++ src/storage.ts
@@ -2,11 +2,11 @@
 async function get(key: string, storage = localStorage): Promise<any | undefined> {
   const data = storage[key]
   if (data === undefined) return
-  return (data[0] === '{') ? JSON.parse(data) : data
+  return (['{', '['].includes(data[0])) ? JSON.parse(data) : data
 }
 
 /* istanbul ignore next */
-async function set(key: string, data: string | Record<string, unknown>, storage = localStorage): Promise<string | Record<string, unknown>> {
+async function set(key: string, data: string | Record<string, unknown> | unknown[], storage = localStorage): Promise<string | Record<string, unknown> | unknown[]> {
   storage[key] = typeof data === 'object' ? JSON.stringify(data) : data
   return data
 }
