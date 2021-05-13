80dc42a063ec8b6954a1277166a560357668acdd
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-05-13 14:29:15 +0200

    export dom as well

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 export * from './arrays'
 export * from './dates'
+export * from './dom'
 export * from './event'
 export * from './functions'
 export * from './numbers'
@@ -9,3 +10,4 @@ export * from './objects'
 export * from './storage'
 export * from './strings'
 export * from './timestamps'
+
