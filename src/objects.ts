9d26cd0e48bd5bd87bb3a3d7629b6dbd26931b35
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-07-14 15:46:18 +0200

    add various utils

--- src/objects.ts
+++ src/objects.ts
@@ -2,8 +2,8 @@ import { delve } from './imports'
 
 /**
  * Return a deep copy of an object or array
- * @param item like : { name : "Pine" }
- * @return item copy like : { name : "Pine" }
+ * @param item like : `{ name : "Pine" }`
+ * @return item copy like : `{ name : "Pine" }`
  */
 export function copy<T> (object: T): T {
   return JSON.parse(JSON.stringify(object))
@@ -11,8 +11,8 @@ export function copy<T> (object: T): T {
 
 /**
  * Return a deep copy of an object or array
- * @param item like : { name : "Pine" }
- * @return item clone like : { name : "Pine" }
+ * @param item like : `{ name : "Pine" }`
+ * @return item clone like : `{ name : "Pine" }`
  */
 export const clone = copy
 
@@ -24,3 +24,59 @@ export const clone = copy
  * @return the nested property value
  */
 export const access = delve
+
+/**
+ * Convert a multi-level deep object into a single-level, flatten, object
+ * @param object like `{ person: { name: 'John Doe', age: 32 } }`
+ * @param path, optional, add a root path to all keys
+ * @returns object like `{ 'person.name': 'John Doe', 'person.age': 32 }`
+ */
+export function flatten<T extends Record<string, unknown>> (object: T, path = ''): T {
+  // eslint-disable-next-line unicorn/no-array-reduce
+  return Object.keys(object).reduce<T>((accumulator: T, key: string): T => {
+    const value = object[key]
+    const newPath = Array.isArray(object) ? `${path}[${key}]` : [path, key].filter(Boolean).join('.')
+    const isObject = [
+      typeof value === 'object',
+      value !== null,
+      !(value instanceof Date),
+      !(value instanceof RegExp),
+      !(Array.isArray(value) && value.length === 0),
+    ].every(Boolean)
+    return isObject ? { ...accumulator, ...flatten(value as Record<string, unknown>, newPath) } : { ...accumulator, [newPath]: value }
+  }, {} as T)
+}
+
+/**
+ * Sort an array of objects by a specific property of theses objects, example :
+ * ```js
+ * persons.sort(byProperty('name'))
+ * ```
+ */
+export function byProperty (property: string, order: 'asc' | 'desc' | '' = ''): (a: Record<string, unknown>, b: Record<string, unknown>) => number {
+  if (order === '') return () => 0
+  const sortOrder = order === 'asc' ? 1 : -1
+  return (a: Record<string, unknown>, b: Record<string, unknown>) => {
+    if (!a[property] && b[property]) return 1 * sortOrder
+    if (a[property] && !b[property]) return -1 * sortOrder
+    const result = ((a[property] as number) < (b[property] as number)) ? -1 : (((a[property] as number) > (b[property] as number)) ? 1 : 0)
+    return result * sortOrder
+  }
+}
+
+/**
+ * Automagically generate styling classes from an object
+ * @param object every key-value will generate a class, ex: { enabled: true, disabled: false, size: 'large' }
+ * @param keys optional, filter the keys to use
+ * @param cls optional, additional classes to add, ex: "add-me"
+ * @returns ready to use string class list, ex: "enabled size-large add-me"
+ */
+export const genClass = (object: Record<string, unknown>, keys: string[] = [], cls: string[] = []): string => {
+  keys = keys.length > 0 ? keys : Object.keys(object)
+  for (const key of keys) {
+    const value = object[key]
+    if (typeof value === 'boolean' && value === true) cls.push(key)
+    else if (typeof value === 'string' && value.length > 0) cls.push(`${key}-${value}`)
+  }
+  return cls.join(' ').trim().replace(/\s+/g, ' ')
+}
--- tests/strings.test.ts
+++ tests/strings.test.ts
@@ -1,10 +1,14 @@
 import { ok, strictEqual as equal } from 'assert'
-import { fillTemplate, getRandomImageUrl, getRandomString, slugify } from '../src'
+import { capitalize, ellipsis, ellipsisWords, fillTemplate, getRandomImageUrl, getRandomString, isJSON, slugify } from '../src'
+import { test } from './utils'
 
 const data = {
   name: 'Wick',
   key_ToHappiness: 'Roo-doo-doot-da-doo',
   quote: 'Bears. Beets. Battlestar Galactica.',
+  details: {
+    pinCode: 3544,
+  },
 }
 const objectIn = { Andy: '{{ key_ToHappiness }} !' }
 const stringOut = `{
@@ -21,13 +25,30 @@ describe('strings', function () {
   })
 
   it('random image', function () { return ok(getRandomImageUrl().length > 0) })
-
   it('random string', function () { return ok(getRandomString().length > 0) })
 
-  it('fill a template string without mustaches and data', function () { return equal(fillTemplate(data.quote), data.quote) })
-  it('fill an empty template string', function () { return equal(fillTemplate(''), '') })
-  it('fill a template string with data', function () { return equal(fillTemplate('John {name}', data), 'John Wick') })
-  it('fill a template string with long key data', function () { return equal(fillTemplate('Andy : {{ key_ToHappiness }} !', data), `Andy : ${data.key_ToHappiness} !`) })
-  it('fill a template string with unknown key', function () { return equal(fillTemplate('John {unknown_key}', data), '') })
-  it('fill a template object with data', function () { return equal(fillTemplate(objectIn, data), stringOut) })
+  test('fill a template string without mustaches and data', fillTemplate(data.quote), data.quote)
+  test('fill an empty template string', fillTemplate(''), '')
+  test('fill a template string with data', fillTemplate('John {name}', data), 'John Wick')
+  test('fill a template string with long key data', fillTemplate('Andy : {{ key_ToHappiness }} !', data), `Andy : ${data.key_ToHappiness} !`)
+  test('fill a template string with unknown key', fillTemplate('John {unknown_key}', data), 'John {unknown_key}')
+  test('fill a template object with data', fillTemplate(objectIn, data), stringOut)
+  test('fill a template string with deep data', fillTemplate('My code is {{details.pinCode}}', data), 'My code is 3544')
+
+  test('capitalize an empty string', capitalize(''), '')
+  test('capitalize a single word', capitalize('hey'), 'Hey')
+  test('capitalize an uppercased word', capitalize('HO'), 'HO')
+  test('capitalize a sentence', capitalize('hello my name is John Doe !'), 'Hello my name is John Doe !')
+
+  test('ellipsis words, giving an empty string', ellipsisWords(''), '')
+  test('ellipsis words, giving a regular sentence', ellipsisWords('Hello my name is Jim Halpert', 5), 'Hello my name is Jim...')
+  test('ellipsis words, giving a short string that should not be processed', ellipsisWords('Hello there'), 'Hello there')
+
+  test('ellipsis, giving an empty string', ellipsis(''), '')
+  test('ellipsis, giving a regular string', ellipsis('I really like pineapples', 18), 'I really like pine...')
+  test('ellipsis, giving a short string that should not be processed', ellipsis('I really like pineapples'), 'I really like pineapples')
+
+  test('valid JSON', isJSON('{ "name": "John Doe" }'), { name: 'John Doe' })
+  test('invalid JSON', isJSON('"name": "John Doe" }'), false)
+  test('un-parse-able JSON', isJSON('{"name" "John Doe" }'), false)
 })
