ba79d59a24cef70afd482bf2b2b95eccfac7e9ce
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-08-01 19:18:29 +0200

    add readableTimeAgo

--- src/dates.ts
+++ src/dates.ts
@@ -42,3 +42,50 @@ export const dateIso10 = (date = new Date()): string => date.toISOString().split
  * @param nbDays the number of days to subtract from today
  */
 export const daysAgoIso10 = (nbDays = 0): string => dateIso10(daysAgo(nbDays))
+
+const MOMENTS = 2
+export const MINUTE = 60
+export const HOUR = 60 * MINUTE
+export const DAY = 24 * HOUR
+export const WEEK = 7 * DAY
+export const MONTH = 30 * DAY
+export const YEAR = 365 * DAY
+
+/**
+ * Make a date readable for us, poor humans
+ * Kudos to https://coolaj86.com/articles/time-ago-in-under-50-lines-of-javascript/
+ * @param date
+ * @returns "a minute ago", "4 days ago"
+ */
+export const readableTimeAgo = (date: Date): string => {
+  let ago = Math.floor(date.getTime() / 1000)
+  let part = 0
+  if (ago < MOMENTS) return 'a moment ago'
+  if (ago < MINUTE) return ago + ' seconds ago'
+  if (ago < (2 * MINUTE)) return 'a minute ago'
+  if (ago < HOUR) {
+    while (ago >= MINUTE) { ago -= MINUTE; part += 1 }
+    return part + ' minutes ago'
+  }
+  if (ago < (2 * HOUR)) return 'an hour ago'
+  if (ago < DAY) {
+    while (ago >= HOUR) { ago -= HOUR; part += 1 }
+    return part + ' hours ago'
+  }
+  if (ago < (2 * DAY)) return 'a day ago'
+  if (ago < WEEK) {
+    while (ago >= DAY) { ago -= DAY; part += 1 }
+    return part + ' days ago'
+  }
+  if (ago < (2 * WEEK)) return 'a week ago'
+  if (ago < MONTH) {
+    while (ago >= WEEK) { ago -= WEEK; part += 1 }
+    return part + ' weeks ago'
+  }
+  if (ago < (2 * MONTH)) return 'a month ago'
+  if (ago < YEAR) {
+    while (ago >= MONTH) { ago -= MONTH; part += 1 }
+    return part + ' months ago'
+  }
+  return 'more than a year ago'
+}
--- tests/dates.test.ts
+++ tests/dates.test.ts
@@ -1,5 +1,5 @@
 import { ok, strictEqual as equal } from 'assert'
-import { dateIso10, dateToIsoString, daysAgo, daysAgoIso10 } from '../src'
+import { dateIso10, dateToIsoString, daysAgo, daysAgoIso10, readableTimeAgo } from '../src'
 
 describe('dates', function () {
   const today = new Date()
@@ -28,5 +28,33 @@ describe('dates', function () {
 
   it('iso string date does not contains timezone if needed', function () {
     ok(!dateToIsoString(new Date(), true).toLowerCase().includes('z'))
+  })
+
+  it('should calculate time ago correctly', function () {
+    [
+      [1.5 * 1000, 'a moment ago'],
+      [10 * 1000, '10 seconds ago'],
+      [59 * 1000, '59 seconds ago'],
+      [60 * 1000, 'a minute ago'],
+      [61 * 1000, 'a minute ago'],
+      [119 * 1000, 'a minute ago'],
+      [120 * 1000, '2 minutes ago'],
+      [121 * 1000, '2 minutes ago'],
+      [(60 * 60 * 1000) - 1000, '59 minutes ago'],
+      [1 * 60 * 60 * 1000, 'an hour ago'],
+      [1.5 * 60 * 60 * 1000, 'an hour ago'],
+      [2.5 * 60 * 60 * 1000, '2 hours ago'],
+      [1.5 * 24 * 60 * 60 * 1000, 'a day ago'],
+      [2.5 * 24 * 60 * 60 * 1000, '2 days ago'],
+      [7 * 24 * 60 * 60 * 1000, 'a week ago'],
+      [14 * 24 * 60 * 60 * 1000, '2 weeks ago'],
+      [27 * 24 * 60 * 60 * 1000, '3 weeks ago'],
+      [28 * 24 * 60 * 60 * 1000, '4 weeks ago'],
+      [29 * 24 * 60 * 60 * 1000, '4 weeks ago'],
+      [1.5 * 30 * 24 * 60 * 60 * 1000, 'a month ago'],
+      [2.5 * 30 * 24 * 60 * 60 * 1000, '2 months ago'],
+      [(12 * 30 * 24 * 60 * 60 * 1000) + 1000, '12 months ago'],
+      [13 * 30 * 24 * 60 * 60 * 1000, 'more than a year ago'],
+    ].forEach(([time, expected]) => equal(readableTimeAgo(new Date(time)), expected))
   })
 })
