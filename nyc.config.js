fcd057a22186495a26796854191a57bac9a1d050
Romain Racamier <romain.racamier@externe.e-i.com>
2021-11-15 11:48:51 +0100

    from js to json config files

--- nyc.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'exclude': ['tests/**', '*.js', 'src/dom.ts'],
-  'reporter': ['text', 'lcovonly'],
-  'check-coverage': true,
-  'branches': 90,
-  'functions': 90,
-  'lines': 90,
-  'statements': 90,
-}
