6994fa4befba5de286e69ca55006c78d2ce13a62
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-12-21 13:38:56 +0100

    add hexToHsl util

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,4 @@
-// Credits : https://github.com/sindresorhus/yoctocolors/blob/main/index.js
-
+// Credits to : https://github.com/sindresorhus/yoctocolors/blob/main/index.js
 export const reset = (string: string) => '\u001B[' + 0 + 'm' + string + '\u001B[' + 0 + 'm'
 export const bold = (string: string) => '\u001B[' + 1 + 'm' + string + '\u001B[' + 22 + 'm'
 export const dim = (string: string) => '\u001B[' + 2 + 'm' + string + '\u001B[' + 22 + 'm'
@@ -29,3 +28,40 @@ export const bgMagenta = (string: string) => '\u001B[' + 45 + 'm' + string + '\u
 export const bgCyan = (string: string) => '\u001B[' + 46 + 'm' + string + '\u001B[' + 49 + 'm'
 export const bgWhite = (string: string) => '\u001B[' + 47 + 'm' + string + '\u001B[' + 49 + 'm'
 export const bgGray = (string: string) => '\u001B[' + 100 + 'm' + string + '\u001B[' + 49 + 'm'
+
+// Credits to https://css-tricks.com/converting-color-spaces-in-javascript/
+/**
+ * Transform a HEX color to HSL
+ * @param hex "#000000"
+ * @returns {string} "hsl(0, 0%, 0%)"
+ */
+export function hexToHsl (hex: string) {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0
+  if (hex.length === 4) {
+    r = Number('0x' + hex[1] + hex[1])
+    g = Number('0x' + hex[2] + hex[2])
+    b = Number('0x' + hex[3] + hex[3])
+  } else if (hex.length === 7) {
+    r = Number('0x' + hex[1] + hex[2])
+    g = Number('0x' + hex[3] + hex[4])
+    b = Number('0x' + hex[5] + hex[6])
+  }
+  // Then to HSL
+  r /= 255
+  g /= 255
+  b /= 255
+  const min = Math.min(r, g, b), max = Math.max(r, g, b), delta = max - min
+  let h = 0, s = 0, l = 0
+  if (delta === 0) h = 0
+  else if (max === r) h = ((g - b) / delta) % 6
+  else if (max === g) h = (b - r) / delta + 2
+  else h = (r - g) / delta + 4
+  h = Math.round(h * 60)
+  if (h < 0) h += 360
+  l = (max + min) / 2
+  s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1))
+  s = Math.round(s * 100)
+  l = Math.round(l * 100)
+  return { h, s, l }
+}
--- tests/colors.test.ts
+++ tests/colors.test.ts
@@ -1,10 +1,15 @@
 import { ok } from 'assert'
 import { test } from 'uvu'
-import { red, yellow } from '../src'
+import { hexToHsl, red, yellow } from '../src'
+import { check } from './utils'
 
 test('colored strings are still strings :)', function () {
   console.log(yellow(':D'))
   ok(red('plop'))
 })
+
+check('color red short : hex => hsl', hexToHsl('#f00'), { h: 0, s: 100, l: 50 })
+check('color red long : hex => hsl', hexToHsl('#ff0000'), { h: 0, s: 100, l: 50 })
+check('color rebeccapurple : hex => hsl', hexToHsl('#663399'), { h: 270, s: 50, l: 40 })
 
 test.run()
