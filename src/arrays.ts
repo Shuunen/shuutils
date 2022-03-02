57fe6d062f06d0ef37bc0d52f10357432f643ef1
Romain Racamier <romain.racamier@externe.e-i.com>
2022-03-02 17:42:02 +0100

    add array shuffle

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,3 +1,4 @@
+import { clone } from './objects'
 
 /**
  * Return an item from an array
@@ -6,4 +7,21 @@
  */
 export function pickOne<T> (items: T[]): T {
   return items[Math.floor(Math.random() * items.length)]
+}
+
+/**
+ * Randomize an array using the Fisher-Yates algorithm
+ * source : https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+ * @param array the array to shuffle
+ * @returns a shuffled clone of the input array
+ */
+export function shuffleArray<T> (input: T[]): T[] {
+  const array = clone(input)
+  for (let indexA = array.length - 1; indexA > 0; indexA--) {
+    const indexB = Math.floor(Math.random() * (indexA + 1))
+    const temporary = array[indexA]
+    array[indexA] = array[indexB]
+    array[indexB] = temporary
+  }
+  return array
 }
--- tests/arrays.test.ts
+++ tests/arrays.test.ts
@@ -1,11 +1,18 @@
 import { ok } from 'assert'
 import { test } from 'uvu'
-import { pickOne } from '../src'
+import { pickOne, shuffleArray } from '../src'
 
 test('pick one', function () {
   const elements = ['damn', 'this', 'test', 'is', 'crazy']
   const element = pickOne(elements)
   ok(elements.includes(element))
+})
+
+test('shuffle an array without affecting the original one', function (){
+  const elements = ['damn', 'this', 'test', 'is', 'crazy']
+  const elementsShuffled = shuffleArray(elements)
+  ok(elements[0] === 'damn' && elements[2] === 'test')
+  ok((elementsShuffled[0] !== 'damn') || (elementsShuffled[2] !== 'test'))
 })
 
 test.run()
