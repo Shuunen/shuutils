aafd9377ef6f76fa86894df00a3b8e6620cdb7f9
Romain Racamier <romain.racamier@externe.e-i.com>
2022-03-02 17:41:49 +0100

    add browser scout

--- src/browser.ts
+++ src/browser.ts
@@ -51,3 +51,60 @@ export const onPageChange = async (callback = (location: string) => { console.lo
   if (current !== last) callback(current)
   onPageChange(callback, wait, current)
 }
+
+// inspired by this script : https://github.com/benbscholz/detect/blob/master/src/detect.js
+class BrowserScout {
+  browser = 'unknown'
+  isIE = false
+  language = 'unknown'
+  os = 'unknown'
+  platform = 'unknown'
+  ua = 'unknown'
+  version = 'unknown'
+  constructor () {
+    this.detect()
+  }
+  private detect () {
+    this.ua = window.navigator.userAgent
+    this.platform = this.getPlatform()
+    this.browser = this.getBrowser()
+    this.isIE = this.browser === 'Internet Explorer'
+    this.version = this.getVersion()
+    this.os = this.getOS()
+    this.language = window.navigator.language
+  }
+  private getBrowser () {
+    if (/MSIE/.test(this.ua) || (/Mozilla/.test(this.ua) && /Trident/.test(this.ua))) return 'Internet Explorer'
+    if (/Edg/.test(this.ua)) return 'Edge'
+    if (/Chrome/.test(this.ua)) return 'Chrome'
+    if (/Opera/.test(this.ua)) return 'Opera'
+    if (/Android/.test(this.ua)) return 'Android'
+    if (/Firefox/.test(this.ua)) return 'Firefox'
+    if (/Safari/.test(this.ua)) return 'Safari'
+    return 'Unknown browser'
+  }
+  private getOS () {
+    if (/Safari/.test(this.ua) && (/iPhone/.test(this.ua) || /iPad/.test(this.ua) || /iPod/.test(this.ua))) return 'iOS'
+    if (this.platform === 'MacIntel' || this.platform === 'MacPPC') return 'Mac OS X'
+    if (this.platform === 'CrOS') return 'ChromeOS'
+    if (this.platform === 'Win32' || this.platform === 'Win64' || /Windows/.test(this.ua)) return 'Windows'
+    if (/Android/.test(this.ua)) return 'Android'
+    if (/Linux/.test(this.platform)) return 'Linux'
+    return 'Unknown OS'
+  }
+  private getPlatform () {
+    if (/Chrome/.test(this.ua) && /CrOS/.test(this.ua)) return 'CrOS'
+    return window.navigator.platform ?? 'Unknown platform'
+  }
+  private getVersion () {
+    if (/MSIE/.test(this.ua)) return (/MSIE \d+\.\d+/.exec(this.ua) ?? [])[0].split(' ')[1]
+    if (/Chrome/.test(this.ua)) return (/Chrome\/[\d.]+/.exec(this.ua) ?? [])[0].split('/')[1]
+    if (/Firefox/.test(this.ua)) return (/Firefox\/[\d.]+/.exec(this.ua) ?? [])[0].split('/')[1]
+    if (/Version/.test(this.ua)) return (/Version\/[\d.]+/.exec(this.ua) ?? [])[0].split('/')[1]
+    if (/rv:(\d+)/.test(this.ua)) return (this.ua.match(/rv:(\d+)/) ?? [])[1]
+    return 'Unknown version'
+  }
+}
+
+export const browserScout = new BrowserScout()
+
