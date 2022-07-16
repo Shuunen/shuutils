2a69505fa1c2276a08797dc8775d69927a6e784a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-07-16 22:13:07 +0200

    build & expose cjs/esm formats

---
+++ bin/build.js
@@ -0,0 +1,21 @@
+import { build } from 'esbuild'
+
+const shared = {
+  bundle: true,
+  outdir: 'dist',
+  platform: 'node',
+}
+
+build({
+  ...shared,
+  entryPoints: ['src/index.ts'],
+  outExtension: { '.js': '.cjs' },
+  format: 'cjs',
+})
+
+build({
+  ...shared,
+  entryPoints: ['src/index.ts', 'src/unique-mark.ts'],
+  outExtension: { '.js': '.esm.js' },
+  format: 'esm',
+})
