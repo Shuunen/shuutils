76b5f775c042ce2a3e12191aead93c0b9428a7e1
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-07-18 22:05:23 +0200

    add arrayUnique, fix #265

--- tests/arrays.test.ts
+++ tests/arrays.test.ts
@@ -15,4 +15,9 @@ test('shuffle an array without affecting the original one', function (){
   ok((elementsShuffled[0] !== 'damn') || (elementsShuffled[2] !== 'test'))
 })
 
+
+check('array unique A', arrayUnique([1, 1, 2, 1, 1, 3, 1]), [1, 2, 3])
+check('array unique B', arrayUnique(['plop', 'plop', 2, 'plop', 'plop', 3, 'plop']), ['plop', 2, 3])
+check('array unique C', arrayUnique([{ name: 'John' }, 'plop', { name: 'John' }, 3, 'plop']), [{ name: 'John' }, 'plop', { name: 'John' }, 3])
+
 test.run()
