fffb14883fb7d9ded20b0de146756634c452bc59
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-09-14 10:35:49 +0200

    enforce debounce tests

--- tests/functions.test.ts
+++ tests/functions.test.ts
@@ -3,12 +3,25 @@ import { equal } from 'uvu/assert'
 import { check, debounce, hasOwnProperty, sleep, throttle } from '../src'
 
 let times: number
+
 /**
- *
+ * @returns {number} the number of times the function has been called
  */
-const myFunction = (): void => {
-  times++
+const myFunction = (): number => {
+  return ++times
 }
+
+const myFunctionDebounced = debounce(myFunction, 100)
+
+/**
+ * @returns {Promise<number>} the number of times the function has been called after 50ms
+ */
+const myAsyncFunction = async (): Promise<number> => {
+  await sleep(50)
+  return ++times
+}
+
+const myAsyncFunctionDebounced = debounce(myAsyncFunction, 100)
 
 /**
  *
@@ -24,9 +37,8 @@ const anAsyncFunctionThatReturnAnObject = async (): Promise<object> => {
   return { name: 'John', age: 30 }
 }
 
-test('debounce', async function () {
+test('debounce A : sync function', async function () {
   times = 0
-  const myFunctionDebounced = debounce(myFunction, 100)
   equal(times, 0)
   myFunctionDebounced()
   equal(times, 0)
@@ -36,6 +48,37 @@ test('debounce', async function () {
   equal(times, 1)
   await sleep(30)
   equal(times, 1)
+})
+
+test('debounce B : async function', async function () {
+  times = 0
+  equal(times, 0)
+  myAsyncFunctionDebounced()
+  equal(times, 0)
+  await sleep(50)
+  equal(times, 0)
+  await sleep(50) // the delay inside the function is 50ms
+  equal(times, 0)
+  await sleep(60)
+  equal(times, 1)
+  await sleep(30)
+  equal(times, 1)
+})
+
+check('debounce C : return type', typeof myFunctionDebounced, 'function')
+
+check('debounce D : return type sync', Object.prototype.toString.call(myFunctionDebounced), '[object AsyncFunction]')
+
+test('debounce E : return type sync resolve', async function () {
+  times = 42
+  equal(await myFunctionDebounced(), 43)
+})
+
+check('debounce F : return type async', Object.prototype.toString.call(myAsyncFunctionDebounced), '[object AsyncFunction]')
+
+test('debounce G : return type async resolve', async function () {
+  times = 42
+  equal(await myAsyncFunctionDebounced(), 43)
 })
 
 test('throttle', async function () {
