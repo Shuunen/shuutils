af96936918ebefe7b8b54ab916560642d02deaf9
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-09-29 15:52:01 +0200

    storage is now sync & get can have a default value

--- src/storage.ts
+++ src/storage.ts
@@ -1,54 +1,62 @@
 import { parseJson } from './strings'
 
+function get<T> (key: string, defaultValue: T, media?: Storage): T
+function get<T> (key: string, defaultValue: undefined, media?: Storage): T | undefined
+/**
+ * Get a value from the storage media
+ * @param key The key of the value to get
+ * @param defaultValue The default value to return if the key is not found
+ * @param media The storage media to use like localStorage, sessionStorage or a custom object
+ * @returns The value or defaultValue if not found
+ */
+function get<T> (key: string, defaultValue: T | undefined, media = localStorage): T | undefined {
+  const path = storage['prefix'] + key
+  const data = media[path] // don't use getItem because it's not supported by all browsers or in memory object storage
+  if (data === undefined || data === null) return defaultValue
+  const { error, value } = parseJson<T>(data)
+  if (error) return data as T // TODO: wait... what ?!
+  return value
+}
+
+/**
+ * Set a value in the storage
+ * @param key The key of the value to set
+ * @param data The value to set
+ * @param media The storage media to use like localStorage, sessionStorage or a custom object
+ * @returns The given value
+ */
+function set<T> (key: string, data: T, media = localStorage): T {
+  const value = typeof data === 'string' ? data : JSON.stringify(data)
+  const path = storage['prefix'] + key
+  media[path] = value // don't use setItem because it's not supported by all browsers or in memory object storage
+  return data
+}
+
+/**
+ * Check if storage has a value
+ * @param key The key of the value to check
+ * @param media The storage media to use like localStorage, sessionStorage or a custom object
+ * @returns true if storage has a value for the given key
+ */
+function has (key: string, media = localStorage): boolean {
+  const value = get(key, undefined, media)
+  return value !== undefined
+}
+
+/**
+ * Remove a value from the storage
+ * @param key The key of the value to remove
+ * @param media The storage media to use like localStorage, sessionStorage or a custom object
+ */
+function clear (key: string, media = localStorage): void {
+  const path = storage['prefix'] + key
+  delete media[path]
+}
+
 export const storage = {
-  /**
-   * Prefix all keys in the storage with a custom string
-   */
-  prefix: '',
-  /**
-   * Get a value from the storage
-   * @param key The key of the value to get
-   * @param storage The storage to use like localStorage, sessionStorage or a custom object
-   * @returns The value or undefined if not found
-   */
-  get: async function get<T> (key: string, storage = localStorage): Promise<T | undefined> {
-    const path = this.prefix + key
-    const data = storage[path] // don't use getItem because it's not supported by all browsers or in memory object storage
-    if (data === undefined || data === null) return
-    const { error, value } = parseJson<T>(data)
-    if (error) return data as T
-    return value
-  },
-  /**
-   * Set a value in the storage
-   * @param key The key of the value to set
-   * @param data The value to set
-   * @param storage The storage to use like localStorage, sessionStorage or a custom object
-   * @returns The given value
-   */
-  set: async function set<T> (key: string, data: T, storage = localStorage): Promise<T> {
-    const value = typeof data === 'string' ? data : JSON.stringify(data)
-    const path = this.prefix + key
-    storage[path] = value // don't use setItem because it's not supported by all browsers or in memory object storage
-    return data
-  },
-  /**
-   * Check if storage has a value
-   * @param key The key of the value to check
-   * @param storage The storage to use like localStorage, sessionStorage or a custom object
-   * @returns true if storage has a value for the given key
-   */
-  has: async function has (key: string, storage = localStorage): Promise<boolean> {
-    const value = await this.get(key, storage)
-    return value !== undefined
-  },
-  /**
-   * Remove a value from the storage
-   * @param key The key of the value to remove
-   * @param storage The storage to use like localStorage, sessionStorage or a custom object
-   */
-  clear: function clear (key: string, storage = localStorage): void {
-    const path = this.prefix + key
-    delete storage[path]
-  },
+  prefix: '', // prefix all keys in the storage with a custom string
+  get,
+  set,
+  has,
+  clear,
 }
--- tests/storage.test.ts
+++ tests/storage.test.ts
@@ -1,4 +1,4 @@
-import { check, checksRun, storage } from '../src'
+import { check, checksRun, hasOwnProperty, storage } from '../src'
 
 const mock = {} as Storage
 type User = { name: string; age: number }
@@ -9,19 +9,28 @@ check('storage set string value for key', storage.set(key, 'Flax', mock), 'Flax'
 check('storage has string value for key', storage.has(key, mock), true)
 
 check('storage set object value for key', storage.set(key, { name: 'Michael', age: 30 }, mock), { name: 'Michael', age: 30 })
-check('storage get object value for key', storage.get<User>(key, mock), { name: 'Michael', age: 30 })
+check('storage get object value for key', storage.get<User>(key, undefined, mock), { name: 'Michael', age: 30 })
 
 check('storage set number value for key', storage.set(key, 30, mock), 30)
-check('storage get number value for key', storage.get<number>(key, mock), 30)
+check('storage get number value for key', storage.get<number>(key, undefined, mock), 30)
 
 check('storage set boolean value for key', storage.set(key, true, mock), true)
-check('storage get boolean value for key', storage.get<boolean>(key, mock), true)
+check('storage get boolean value for key', storage.get<boolean>(key, undefined, mock), true)
 
 check('storage set array value for key', storage.set(key, [1, 2, 3], mock), [1, 2, 3])
-check('storage get array value for key', storage.get<number[]>(key, mock), [1, 2, 3])
+check('storage get array value for key', storage.get<number[]>(key, undefined, mock), [1, 2, 3])
 
 check('storage clear value for key', storage.clear(key, mock))
 
-check('storage get return undefined when key is not found', storage.get<User>('John', mock))
+check('storage get return undefined when key is not found', storage.get<User>('John', undefined, mock))
+
+check('storage get default value when key is not found', storage.get<User>('John', { name: 'John', age: 30 }, mock), { name: 'John', age: 30 })
+
+storage.prefix = 'test-'
+check('storage prefix is set', storage.prefix, 'test-')
+check('storage set string value for key with prefix', storage.set(key, 'Hudson', mock), 'Hudson')
+check('storage get string value for key with prefix', storage.get<string>(key, undefined, mock), 'Hudson')
+check('storage contains a prefixed key', hasOwnProperty(mock, 'test-Michael'), true)
+check('storage clear value for key with prefix', storage.clear(key, mock))
 
 checksRun()
