c2037b3fada65e3801d944d994e493292b96fea2
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-05-07 19:19:13 +0200

    add isTestEnvironment util

---
+++ src/environment.ts
@@ -0,0 +1,7 @@
+/**
+ * Check if the environment is a test environment
+ * @returns true if the environment is a test environment
+ */
+export function isTestEnvironment () {
+  return process.env.NODE_ENV === 'test'
+}
