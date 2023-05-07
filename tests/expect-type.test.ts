caa8a1a0f40aeba9a6fc85188b31cc6087dfe92a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-05-07 16:47:25 +0200

    remove eslint disable rules

--- tests/expect-type.test.ts
+++ tests/expect-type.test.ts
@@ -7,60 +7,60 @@ interface Person {
 }
 
 it('expectType A valid', () => {
-  expect(expectType<number>(12)).toBe(true)
+  expect(expectType<number>(12)).toBeTruthy()
 })
 
 it('expectType B invalid', () => {
   // @ts-expect-error invalid type here
-  expect(expectType<number>('12')).toBe(true)
+  expect(expectType<number>('12')).toBeTruthy()
 })
 
 it('expectType C complex', () => {
-  expect(expectType<Person>({ name: 'John', age: 42 })).toBe(true)
+  expect(expectType<Person>({ name: 'John', age: 42 })).toBeTruthy()
 })
 
 it('expectType D complex invalid', () => {
   // @ts-expect-error invalid type here
-  expect(expectType<Person>({ name: 'John', age: '42' })).toBe(true)
+  expect(expectType<Person>({ name: 'John', age: '42' })).toBeTruthy()
 })
 
 it('expectEqualTypes A inferred valid', () => {
-  expect(expectEqualTypes(12, 34)).toBe(true)
+  expect(expectEqualTypes(12, 34)).toBeTruthy()
 })
 
 it('expectEqualTypes B inferred invalid', () => {
   // @ts-expect-error invalid type here
-  expect(expectEqualTypes(12, '12')).toBe(true)
+  expect(expectEqualTypes(12, '12')).toBeTruthy()
 })
 
 it('expectEqualTypes C inferred complex valid', () => {
-  expect(expectEqualTypes({ name: 'John', age: 42 }, { name: 'Johnny', age: 6 })).toBe(true)
+  expect(expectEqualTypes({ name: 'John', age: 42 }, { name: 'Johnny', age: 6 })).toBeTruthy()
 })
 
 it('expectEqualTypes D inferred complex invalid', () => {
   // @ts-expect-error invalid type here
-  expect(expectEqualTypes({ name: 'John', age: 42 }, { name: 'Johnny', age: '66' })).toBe(true)
+  expect(expectEqualTypes({ name: 'John', age: 42 }, { name: 'Johnny', age: '66' })).toBeTruthy()
 })
 
 it('expectEqualTypes E specified valid', () => {
-  expect(expectEqualTypes<number, number>(12, 35)).toBe(true)
+  expect(expectEqualTypes<number, number>(12, 35)).toBeTruthy()
 })
 
 it('expectEqualTypes F specified invalid', () => {
   // @ts-expect-error invalid type here
-  expect(expectEqualTypes<number, number>(12, '41')).toBe(true)
+  expect(expectEqualTypes<number, number>(12, '41')).toBeTruthy()
 })
 
 it('expectEqualTypes G specified complex valid', () => {
-  expect(expectEqualTypes<Person, Person>({ name: 'John', age: 42 }, { name: 'Johnny', age: 31 })).toBe(true)
+  expect(expectEqualTypes<Person, Person>({ name: 'John', age: 42 }, { name: 'Johnny', age: 31 })).toBeTruthy()
 })
 
 it('expectEqualTypes H specified complex invalid', () => {
   // @ts-expect-error invalid type here
-  expect(expectEqualTypes<Person, Person>({ name: 'John', age: 42 }, { name: 'Johnny', age: '31' })).toBe(true)
+  expect(expectEqualTypes<Person, Person>({ name: 'John', age: 42 }, { name: 'Johnny', age: '31' })).toBeTruthy()
 })
 
 it('expectEqualTypes I specified type mismatch', () => {
   // @ts-expect-error invalid type here
-  expect(expectEqualTypes<Person, string>({ name: 'John', age: 42 }, 'Johnny')).toBe(true)
+  expect(expectEqualTypes<Person, string>({ name: 'John', age: 42 }, 'Johnny')).toBeTruthy()
 })
