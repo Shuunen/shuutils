9f4c908338fd4caecdf077d7142a65f246704832
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-06-10 19:08:24 +0200

    enforce media usage

--- src/events.ts
+++ src/events.ts
@@ -1,8 +1,20 @@
 /* c8 ignore start */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
+type ListenerMedia = Element | HTMLElement | Window
+
+/**
+ * Get the default media, window
+ * @returns the media, window by default
+ */
+function getMedia () {
+  if (typeof window === 'undefined') throw new Error('no media provided & no window available')
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  return window satisfies ListenerMedia as ListenerMedia
+}
+
 export interface Listener {
-  callback: (event: any) => any
-  media: any
+  callback: (event: unknown) => unknown
+  media: ListenerMedia
   name: string
 }
 
@@ -11,21 +23,10 @@ export interface Listener {
  * @param name the name of the event
  * @param data the data to emit
  * @param media the media to emit the event from, like window or a dom element
- * @returns true if the event is emitted
  */
-export function emit<Data> (name: string, data?: Readonly<Data>, media?: Element | HTMLElement | Window) {
-  let targetMedia = media
-  if (targetMedia === undefined) {
-    if (typeof window === 'undefined') {
-      // eslint-disable-next-line no-console
-      console.error('no media provided & no window available')
-      return false
-    }
-    targetMedia = window
-  }
-  if (data === undefined) targetMedia.dispatchEvent(new CustomEvent(name))
-  else targetMedia.dispatchEvent(new CustomEvent(name, { detail: data }))
-  return true
+export function emit<Data> (name: string, data?: Readonly<Data>, media = getMedia()) {
+  if (data === undefined) media.dispatchEvent(new CustomEvent(name))
+  else media.dispatchEvent(new CustomEvent(name, { detail: data }))
 }
 
 /**
@@ -35,17 +36,7 @@ export function emit<Data> (name: string, data?: Readonly<Data>, media?: Element
  * @param media the media to listen to the event, like window or a dom element
  * @returns false if the event cannot be not listened to or a listener object if it can
  */
-export function on<Data> (name: string, callback: (data: Data, event: Event) => unknown, media?: Element | HTMLElement | Window) {
-  let targetMedia = media
-  if (targetMedia === undefined) {
-    if (typeof window === 'undefined') {
-      // eslint-disable-next-line no-console
-      console.error('no media provided & no window available')
-      return false
-    }
-    targetMedia = window
-  }
-
+export function on<Data> (name: string, callback: (data: Data, event: Event) => unknown, media = getMedia()) {
   /**
    * The callback to call when the event is emitted
    * @param event the event
@@ -55,20 +46,18 @@ export function on<Data> (name: string, callback: (data: Data, event: Event) => 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/consistent-type-assertions
     return callback(event instanceof CustomEvent ? event.detail : event, event as Event)
   }
-  targetMedia.addEventListener(name, onCallback)
+  media.addEventListener(name, onCallback)
   return { callback: onCallback, media, name }
 }
 
 /**
  * Remove a listener from a media, window by default
- * @param root0 the listener to remove
- * @param root0.media the media to remove the listener from, like window or a dom element
- * @param root0.name the name of the event to remove the listener from
- * @param root0.callback the callback to remove
+ * @param listener the listener to remove
+ * @param listener.media the media to remove the listener from, like window or a dom element
+ * @param listener.name the name of the event to remove the listener from
+ * @param listener.callback the callback to remove
  */
 export function off ({ callback, media, name }: Readonly<Listener>) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   media.removeEventListener(name, callback)
 }
-
 /* c8 ignore stop */
